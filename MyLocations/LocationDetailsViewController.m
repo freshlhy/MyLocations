5511fa222b880deb6ccefafa2e9276f9884ae53f
freshlhy <freshlhy@gmail.com>
2014-03-18 02:06:27 -0700

    UI Improvements

--- MyLocations/LocationDetailsViewController.m
+++ MyLocations/LocationDetailsViewController.m
@@ -8,6 +8,7 @@
 
 #import "LocationDetailsViewController.h"
 #import "CategoryPickerViewController.h"
+#import "HudView.h"
 
 @interface LocationDetailsViewController () <UITextViewDelegate>
 @property(weak, nonatomic) IBOutlet UITextView *descriptionTextView;
@@ -34,7 +35,11 @@
 
 - (IBAction)done:(id)sender {
   NSLog(@"Description '%@'", _descriptionText);
-  [self closeScreen];
+  HudView *hudView =
+      [HudView hudInView:self.navigationController.view animated:YES];
+  hudView.text = @"Tagged!";
+
+  [self performSelector:@selector(closeScreen) withObject:nil afterDelay:0.8];
 }
 
 - (IBAction)cancel:(id)sender {
@@ -63,6 +68,21 @@
   }
 
   self.dateLabel.text = [self formatDate:[NSDate date]];
+
+  UITapGestureRecognizer *gestureRecognizer =
+      [[UITapGestureRecognizer alloc] initWithTarget:self
+                                              action:@selector(hideKeyboard:)];
+  gestureRecognizer.cancelsTouchesInView = NO;
+  [self.tableView addGestureRecognizer:gestureRecognizer];
+}
+
+- (void)hideKeyboard:(UIGestureRecognizer *)gestureRecognizer {
+  CGPoint point = [gestureRecognizer locationInView:self.tableView];
+  NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:point];
+  if (indexPath != nil && indexPath.section == 0 && indexPath.row == 0) {
+    return;
+  }
+  [self.descriptionTextView resignFirstResponder];
 }
 
 - (NSString *)stringFromPlacemark:(CLPlacemark *)placemark {
@@ -102,6 +122,21 @@
     return self.addressLabel.frame.size.height + 20;
   } else {
     return 44;
+  }
+}
+
+- (NSIndexPath *)tableView:(UITableView *)tableView
+    willSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+  if (indexPath.section == 0 || indexPath.section == 1) {
+    return indexPath;
+  } else {
+    return nil;
+  }
+}
+- (void)tableView:(UITableView *)tableView
+    didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+  if (indexPath.section == 0 && indexPath.row == 0) {
+    [self.descriptionTextView becomeFirstResponder];
   }
 }
 
